d69c7c3c8a88d1925201f178c2c5bb68a23b0c73
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2016-06-01 09:55:12 +0200

    Set up project

---
+++ src/charlie/compiler.h
@@ -0,0 +1,53 @@
+/* 
+ * Copyright (c) 2016, Matthias Lochbrunner <matthias_lochbrunner@live.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef  CHARLIE_COMPILER_H
+#define CHARLIE_COMPILER_H
+
+#include <string>
+#include <functional>
+#include "exportDefs.h"
+
+using namespace std;
+
+namespace charlie {
+
+	class Compiler {
+	public:
+		xprt Compiler();
+		xprt Compiler(function<void(string const &message)> messageDelegate);
+		xprt void Compile(string const &filename);
+
+	private:
+		function<void(string const &message)> _messageDelegate;
+
+		void log(string const &message);
+	};
+
+}
+
+#endif // ! CHARLIE_COMPILER_H
